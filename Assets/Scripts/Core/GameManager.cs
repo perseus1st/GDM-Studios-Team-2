2020039ad9ad8410f0b5cb0a4b88437f027e6d60
VICTOIRE BERGERON <victoire.bergeron@mail.mcgill.ca>
2026-01-15 20:26:19 -0500

    created GameManager script for keeping game state logic

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,10 +1,22 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager Instance;  //allows to reference game manager from anywhere 
+    public GameState State; 
+    public static event Action<GameState> OnGameStateChanged; 
+
+  void Awake()
     {
+        Instance = this; 
+    }
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+    {
+        UpdateGameState(GameState.Options); 
+
         Debug.Log("Hello world"); 
     }
 
@@ -13,4 +25,37 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         
     }
+
+    public void UpdateGameState(GameState newState)
+    {
+        State = newState; 
+
+        //TODO write specific logic for each state 
+        switch (newState)
+        {
+            case GameState.Options:
+                break;
+            case GameState.Paused: 
+                break; 
+            case GameState.Cutscene:
+                break; 
+            case GameState.Minigame: 
+                break; 
+            case GameState.Room:
+                break; 
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null); 
+        }
+
+        OnGameStateChanged?.Invoke(newState); 
+    }
+}
+
+public enum GameState
+{
+    Options,
+    Paused, 
+    Cutscene, 
+    Room,
+    Minigame 
 }
