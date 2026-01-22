5d1e2c1fc666a6eaec1855372fdc5439c8b3e195
KianZhu <kaiyang.zhu@outlook.com>
2026-01-21 20:42:37 -0500

    Implemented sister's room objects and functional navigation

---
+++ Assets/Scripts/MC-And-Sister-Room/Kite.cs
@@ -0,0 +1,39 @@
+
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kite : MonoBehaviour, IInteractable
+{
+    private Transform interactIcon;
+
+    void Start()
+    {
+        interactIcon = transform.Find("InteractIcon");
+        interactIcon.LookAt(Camera.main.transform.position);
+        interactIcon.gameObject.SetActive(false);
+    }
+
+    public void Interact()
+    {
+        SceneManager.LoadScene("Kite_Minigame");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            player.SetInteractable(this);
+            interactIcon.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerController player))
+        {
+            player.SetInteractable(null);
+            interactIcon.gameObject.SetActive(false);
+        }
+    }
+}
